f06339086f39435d52d9db27cde7a92c020b53e1
Feoktist92 <feoktist92@gmail.com>
2023-07-16 22:08:22 +0800

    added url repo

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,12 +1,19 @@
 function Footer() {
-    return <footer className="page-footer light-blue darken-1">
-    <div className="footer-copyright light-blue darken-1">
-      <div className="container">
-      © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="!#">Repository</a>
-      </div>
-    </div>
-  </footer>
+    return (
+        <footer className='page-footer light-blue darken-1'>
+            <div className='footer-copyright light-blue darken-1'>
+                <div className='container'>
+                    © {new Date().getFullYear()} Copyright Text
+                    <a
+                        className='grey-text text-lighten-4 right'
+                        href='https://github.com/Feoktist92/react-movies'
+                    >
+                        Repository
+                    </a>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
-export {Footer}
+export { Footer };
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,12 +1,20 @@
 function Header() {
-    return <nav className="light-blue lighten-2">
-    <div className="nav-wrapper">
-      <a href="!#" className="brand-logo">React Movies</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="!#">Repository</a></li>
-      </ul>
-    </div>
-  </nav>
+    return (
+        <nav className='light-blue lighten-2'>
+            <div className='nav-wrapper'>
+                <a href='!#' className='brand-logo'>
+                    React Movies
+                </a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li>
+                        <a href='https://github.com/Feoktist92/react-movies'>
+                            Repository
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
 
-export {Header}
+export { Header };
