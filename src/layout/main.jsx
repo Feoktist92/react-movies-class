0ece8f439270d8c5d16769a9c3e851c7056501f9
Feoktist92 <feoktist92@gmail.com>
2023-07-16 21:57:43 +0800

    changed protocol http to https

--- src/layout/main.jsx
+++ src/layout/main.jsx
@@ -11,24 +11,32 @@ class Main extends React.Component {
         loading: true,
     };
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
-            );
+            )
+            .catch((error) => {
+                console.error(error);
+                this.setState({ loading: false });
+            });
     }
 
     searchMovies = (str, type = 'all') => {
         this.setState({ loading: true });
         fetch(
-            `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
                 type !== 'all' ? `&type=${type}` : ''
             }`
         )
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
-            );
+            )
+            .catch((error) => {
+                console.error(error);
+                this.setState({ loading: false });
+            });
     };
 
     render() {
