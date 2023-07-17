7c39ccb181aa6cebb15e2f94a66f8107ef23cbf2
Feoktist92 <feoktist92@gmail.com>
2023-07-17 18:47:27 +0800

    project completed

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -2,25 +2,37 @@ import React from 'react';
 
 function Movie(props) {
     const {
-    Title: title,
-    Year: year,
-    ImdbID: id,
-    Type: type,
-    Poster: poster,
+        Title: title,
+        Year: year,
+        ImdbID: id,
+        Type: type,
+        Poster: poster,
     } = props;
 
-    return <div className="movie card" id={id}>
-    <div className="card-image waves-effect waves-block waves-light">
-        {
-        poster === 'N/A' ? <img className="activator" src={`https://placehold.co/300x450?text=${title}`} alt='poster'/>
-        : <img className="activator" src={poster} alt='poster'/>
-        }
-    </div>
-    <div className="card-content">
-      <span className="card-title activator grey-text text-darken-4">{title}</span>
-      <p>{year}<span className='right'>{type}</span></p>
-    </div>
-    </div>;
+    return (
+        <div className='movie card' id={id}>
+            <div className='card-image waves-effect waves-block waves-light'>
+                {poster === 'N/A' ? (
+                    <img
+                        className='activator'
+                        src={`https://placehold.co/300x450?text=${title}`}
+                        alt='poster'
+                    />
+                ) : (
+                    <img className='activator' src={poster} alt='poster' />
+                )}
+            </div>
+            <div className='card-content'>
+                <span className='card-title activator grey-text text-darken-4'>
+                    {title}
+                </span>
+                <p>
+                    {year}
+                    <span className='right'>{type}</span>
+                </p>
+            </div>
+        </div>
+    );
 }
 
 export { Movie };
