e1abfd06ef09a57fa9d2f6c727e33d36cc889c4a
Feoktist92 <feoktist92@gmail.com>
2023-07-17 18:03:19 +0800

    changes index.css

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -2,7 +2,10 @@ function Header() {
     return (
         <nav className='light-blue lighten-2'>
             <div className='nav-wrapper'>
-                <a href='!#' className='brand-logo'>
+                <a
+                    href='https://feoktist92.github.io/react-movies/'
+                    className='brand-logo'
+                >
                     React Movies
                 </a>
                 <ul id='nav-mobile' className='right hide-on-med-and-down'>
